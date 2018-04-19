28e00ca3360e9faec9d3f5c449e4cd0904e7e3a4
rockon798 <k0935731736@gmail.com>
2018-04-19 22:06:47 +0800

    1

---
+++ Assets/move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        rigidbody = GetComponent<Rigidbody>();
+	}
+
+    public float speed;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    Rigidbody rigidbody;
+
+    float nextFire;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * speed;
+    }
+}
